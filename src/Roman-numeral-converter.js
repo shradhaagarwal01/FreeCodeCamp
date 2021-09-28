39cd2fbdf6918fcaaa3e4f970d2d40268e4a7a0b
Shradha Agarwal <shradhaagarwal386@gmail.com>
2021-09-28 19:25:36 +0530

    Create Roman-numeral-converter.js

---
+++ src/Roman-numeral-converter.js
@@ -0,0 +1,14 @@
+function convertToRoman(num) {
+  var m = ["", "M", "MM", "MMM"];
+  var c = ["", "C", "CC", "CCC", "CD", "D","DC","DCC", "DCCC", "CM "];
+  var  x = ["","X","XX","XXX","XL", "L", "LX", "LXX", "LXXX", "XC"];
+  var  i = ["","I","II","III","IV","V", "VI", "VII", "VIII", "IX"];
+  var thousands = m[Math.floor(num/1000)];
+  var hundereds = c[Math.floor((num%1000)/100)];
+  var tens = x[Math.floor((num%100)/10)];
+  var ones = i[num%10];
+  var ans = thousands + hundereds + tens + ones;
+  return ans.replace(/ /g,'');
+}
+
+convertToRoman(36);
